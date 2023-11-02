0d84f212551ce06e0546bd98f1b6a0d4ff2e85cb
SillyFrog36 <sirrpk4@gmail.com>
2023-11-02 16:44:30 -0400

    Calculator with if else logic

--- src/Test1.java
+++ src/Test1.java
@@ -8,26 +8,15 @@ public class Test1 {
 
     public static double basicCalculator(final Operation operation, final int num1, final int num2){
         double result = 0;
-
-        switch(operation) {
-            case ADD:
-                // if operation = ADD, return the sum
-                result = num1 + num2;
-                break;
-            case SUBTRACT:
-                result = num1 - num2;
-                break;
-            case MULTIPLY:
-                result = num1 * num2;
-                break;
-            case DIVIDE:
-                if (num2 == 0) {
-                    System.out.println("Can't divide by 0");
-                } else {
-                    result = ((double)num1) / num2;
-                }
-                break;
-        }
+        if (operation.compareTo(Operation.ADD) == 0) {
+            result = num1 + num2;
+        }else if (operation.compareTo(Operation.SUBTRACT) == 0) {
+            result = num1 - num2;
+        } else if (operation.compareTo(Operation.MULTIPLY) == 0) {
+            result = num1 * num2;
+        } else if (operation.compareTo(Operation.DIVIDE) == 0) {
+            result = num1 / ((double)num2);
+        } 
         return result;
     }
 
